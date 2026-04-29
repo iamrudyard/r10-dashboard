3275c86be83fc389a44335c9f7abba8f777e5364
iamrudyard <147968558+iamrudyard@users.noreply.github.com>
2026-04-30 07:30:50 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-        'process.env.PUBLIC_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL || env.VITE_SUPABASE_PUBLISHABLE_KEY)
-  }
 })
